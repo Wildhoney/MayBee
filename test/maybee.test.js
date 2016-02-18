8d492123f453ec8aee4e6e534c1a7d1016bdb5da
Wildhoney <adam.timberlake@gmail.com>
2016-02-18 08:22:23 +0000

    Added describe to tests

--- test/maybee.test.js
+++ test/maybee.test.js
@@ -27,56 +27,60 @@ const person = {
     }
 };
 
-it('Should be able to throw an exception when safeguarding a non-object;', () => {
-    expect(() => safeguard('Adam')).toThrow(new Error('MayBee: Cannot safeguard non-objects.'));
-    expect(() => safeguard(30)).toThrow(new Error('MayBee: Cannot safeguard non-objects.'));
-    expect(() => safeguard(Symbol('Kipper'))).toThrow(new Error('MayBee: Cannot safeguard non-objects.'));
-});
+describe('MayBee', () => {
 
-it('Should be able to access properties on the object;', () => {
+    it('Should be able to throw an exception when safeguarding a non-object;', () => {
+        expect(() => safeguard('Adam')).toThrow(new Error('MayBee: Cannot safeguard non-objects.'));
+        expect(() => safeguard(30)).toThrow(new Error('MayBee: Cannot safeguard non-objects.'));
+        expect(() => safeguard(Symbol('Kipper'))).toThrow(new Error('MayBee: Cannot safeguard non-objects.'));
+    });
 
-    const model = safeguard(person);
+    it('Should be able to access properties on the object;', () => {
 
-    expect(model.name).toEqual('Adam');
-    expect(model.age).toEqual(30);
-    expect(model.type).toEqual(PERSON_TYPE);
-    expect(model.createdDateTime instanceof Date).toBeTruthy();
+        const model = safeguard(person);
 
-    expect(model.pets[0].name).toEqual('Kipper');
-    expect(model.pets[1].name).toEqual('Busters');
-    expect(model.pets[2].name).toEqual('Miss Kittens');
+        expect(model.name).toEqual('Adam');
+        expect(model.age).toEqual(30);
+        expect(model.type).toEqual(PERSON_TYPE);
+        expect(model.createdDateTime instanceof Date).toBeTruthy();
 
-    expect(model.pets[0].type).toEqual(TYPE_CAT);
-    expect(model.pets[1].type).toEqual(TYPE_CAT);
-    expect(model.pets[2].type).toEqual(TYPE_CAT);
+        expect(model.pets[0].name).toEqual('Kipper');
+        expect(model.pets[1].name).toEqual('Busters');
+        expect(model.pets[2].name).toEqual('Miss Kittens');
 
-    expect(model.petNames()).toEqual('Kipper, Busters, Miss Kittens');
-    expect(model.names.getNameAndAge()).toEqual('Adam is 30 years old');
-    expect(model.names.customNameAndAge('Maria')(24)).toEqual('Maria is 24 years old');
+        expect(model.pets[0].type).toEqual(TYPE_CAT);
+        expect(model.pets[1].type).toEqual(TYPE_CAT);
+        expect(model.pets[2].type).toEqual(TYPE_CAT);
 
-});
+        expect(model.petNames()).toEqual('Kipper, Busters, Miss Kittens');
+        expect(model.names.getNameAndAge()).toEqual('Adam is 30 years old');
+        expect(model.names.customNameAndAge('Maria')(24)).toEqual('Maria is 24 years old');
 
-it('Should be able to deduce when properties are undefined/null;', () => {
+    });
 
-    const model = safeguard(person);
+    it('Should be able to deduce when properties are undefined/null;', () => {
 
-    expect(isUndefined(model.location)).toBeTruthy();
-    expect(isUndefined(model.born.in.which.country)).toBeTruthy();
-    expect(isNull(model.born)).toBeTruthy();
+        const model = safeguard(person);
 
-    expect(isUndefined(model.name)).toBeFalsy();
-    expect(isNull(model.age)).toBeFalsy();
-    expect(isNull(Object.create(null))).toBeFalsy();
+        expect(isUndefined(model.location)).toBeTruthy();
+        expect(isUndefined(model.born.in.which.country)).toBeTruthy();
+        expect(isNull(model.born)).toBeTruthy();
 
-});
+        expect(isUndefined(model.name)).toBeFalsy();
+        expect(isNull(model.age)).toBeFalsy();
+        expect(isNull(Object.create(null))).toBeFalsy();
 
-it('Should not throw type errors when chaining properties and functions;', () => {
+    });
 
-    const model = safeguard(person);
+    it('Should not throw type errors when chaining properties and functions;', () => {
 
-    expect(isUndefined(model.property.does.not.exist)).toBeTruthy();
-    expect(isUndefined(model.function.does.not.exist())).toBeTruthy();
-    expect(isUndefined(model.property().does.not.exist)).toBeTruthy();
-    expect(isUndefined(model.function().does().not().exist())).toBeTruthy();
+        const model = safeguard(person);
 
+        expect(isUndefined(model.property.does.not.exist)).toBeTruthy();
+        expect(isUndefined(model.function.does.not.exist())).toBeTruthy();
+        expect(isUndefined(model.property().does.not.exist)).toBeTruthy();
+        expect(isUndefined(model.function().does().not().exist())).toBeTruthy();
+
+    });
+    
 });
